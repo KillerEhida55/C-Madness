42cccd322471ed81e90874493c917dafcfe3b160
KillerEhida55 <aromietheanimator@gmail.com>
2024-10-26 23:19:19 +0500

    proverka

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true) {
+           double firstvalue, secondvalue;
+        string symbol;
+
+        Console.WriteLine("Введите первое число:");
+        firstvalue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите действие:");
+        symbol = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число:");
+        secondvalue = double.Parse(Console.ReadLine());
+
+        if (symbol == "+") {
+            Console.WriteLine(firstvalue + secondvalue);
+        }
+        else if (symbol == "-") {
+            Console.WriteLine(firstvalue - secondvalue);
+        }
+        else if (symbol == "*") {
+            Console.WriteLine(firstvalue * secondvalue);
+        }
+        else if (symbol == "/") {
+            if (firstvalue == 0 || secondvalue == 0) {
+                Console.WriteLine("Неправильно");
+            }
+            else {
+                Console.WriteLine(firstvalue / secondvalue);
+            }
+        }
+        Console.ReadLine();
+        Console.Clear(); 
+        }
+    }
+}
